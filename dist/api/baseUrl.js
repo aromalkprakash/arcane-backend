05c279056c2ea6bd3b3404140ab6d6fabc5c54f3
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-08-31 13:46:11 +0530

    fin

--- dist/api/baseUrl.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// export const Movie_url = "http://localhost:3333/api/movie";
-// export const User_url = "http://localhost:8080/api/users";
-// export const Review_url = "http://localhost:2255/api/review";
-// // copy
